ed90db824dd7578c0125d529bc900d2932197b53
DevSammyKad <DevSammyKad@gmail.com>
2025-01-21 13:31:54 +0530

    upgrade react -19

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider dynamic>
+    <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body className={GeistSans.className}>
           <GoogleOneTap />
