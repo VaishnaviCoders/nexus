510bfad7f37d3236b91a318aeb8c75cc18154204
DevSammyKad <DevSammyKad@gmail.com>
2025-02-02 11:17:48 +0530

    fix: Notification Button ref

--- app/components/dashboardComponents/NotificationFeed.tsx
+++ app/components/dashboardComponents/NotificationFeed.tsx
@@ -7,7 +7,7 @@ import {
   NotificationIconButton,
   NotificationFeedPopover,
 } from '@knocklabs/react';
-import { JSXElementConstructor, ReactElement } from 'react';
+
 // Required CSS import, unless you're overriding the styling
 import '@knocklabs/react/dist/index.css';
 // import '../../notification-feed-overrides.css';
@@ -15,37 +15,32 @@ import { useUser } from '@clerk/nextjs';
 
 const NotificationFeed = () => {
   const [isVisible, setIsVisible] = useState(false);
-  const notifButtonRef = useRef<HTMLElement | null>(null);
+  const notifButtonRef = useRef<HTMLButtonElement>(null);
 
   const { user } = useUser();
   if (!user) return;
 
-  console.log(
-    process.env.PUBLIC_KNOCK_API_KEY,
-    process.env.NEXT_PUBLIC_KNOCK_FEED_CHANNEL_ID
-  );
-
-  const NotificationIconButtonComponent =
-    NotificationIconButton as JSXElementConstructor<any>;
-
   return (
     <KnockProvider
-      apiKey={String(process.env.NEXT_PUBLIC_KNOCK_API_KEY)}
+      apiKey={process.env.NEXT_PUBLIC_KNOCK_API_KEY || ''}
       userId={user.id}
     >
       <KnockFeedProvider
         feedId={String(process.env.NEXT_PUBLIC_KNOCK_FEED_CHANNEL_ID)}
       >
         <>
-          <NotificationIconButtonComponent
-            ref={notifButtonRef as React.RefObject<HTMLButtonElement>}
-            onClick={(e: any) => setIsVisible(!isVisible)}
+          <NotificationIconButton
+            ref={notifButtonRef}
+            onClick={() => setIsVisible(!isVisible)}
           />
-          <NotificationFeedPopover
-            buttonRef={notifButtonRef as React.RefObject<HTMLElement>}
-            isVisible={isVisible}
-            onClose={() => setIsVisible(false)}
-          />
+
+          {notifButtonRef.current && (
+            <NotificationFeedPopover
+              buttonRef={notifButtonRef as React.RefObject<HTMLButtonElement>}
+              isVisible={isVisible}
+              onClose={() => setIsVisible(false)}
+            />
+          )}
         </>
       </KnockFeedProvider>
     </KnockProvider>
