bdf4402723a62ea05e7af257069139b50f6318f3
DevSammyKad <DevSammyKad@gmail.com>
2025-07-25 20:45:23 +0530

    added fees reminder and fix fees features

--- app/(website)/test/page.tsx
+++ app/(website)/test/page.tsx
@@ -1,63 +1,7 @@
-'use server';
+import React from 'react';
 
-import { Feedback } from '@/components/Feedback';
-import prisma from '@/lib/db';
-import {
-  NotificationChannel,
-  NotificationStatus,
-} from '@/lib/generated/prisma';
-import { getOrganizationId } from '@/lib/organization';
-import { calculateNotificationCost, formatCurrencyIN } from '@/lib/utils';
-
-async function test() {
-  const organization = await prisma.academicYear.findMany();
-  return organization;
-}
-
-export async function getOrganizationTotalCost(
-  organizationId: string
-): Promise<number> {
-  const result = await prisma.notificationLog.aggregate({
-    where: {
-      organizationId,
-      status: {
-        in: [NotificationStatus.SENT, NotificationStatus.DELIVERED],
-      },
-    },
-    _sum: {
-      cost: true,
-    },
-  });
-
-  return Number(result._sum.cost || 0);
-}
-
-const page = async () => {
-  const organizationId = await getOrganizationId();
-
-  const data = await getOrganizationTotalCost(organizationId);
-
-  console.log('data', data);
-
-  const logs = await prisma.notificationLog.findMany({
-    where: {
-      organizationId,
-    },
-    select: {
-      cost: true,
-    },
-  });
-
-  const totalCostInRupees = logs.reduce((acc, log) => acc + log.cost, 0);
-
-  console.log('Total Cost (₹):', totalCostInRupees);
-
-  return (
-    <div className="flex justify-center items-center h-screen">
-      cost {formatCurrencyIN(totalCostInRupees)}
-      <Feedback />
-    </div>
-  );
+const page = () => {
+  return <div>page</div>;
 };
 
 export default page;
