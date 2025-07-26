ecd5d3dc9ebbc98cec9dadd028d370b296776123
DevSammyKad <DevSammyKad@gmail.com>
2025-07-26 14:56:00 +0530

    remove driverAdapters from prisma

--- generated/prisma/internal/prismaNamespace.ts
+++ generated/prisma/internal/prismaNamespace.ts
@@ -3468,10 +3468,6 @@ export interface PrismaClientOptions {
     isolationLevel?: TransactionIsolationLevel
   }
   /**
-   * Instance of a Driver Adapter, e.g., like one provided by `@prisma/adapter-planetscale`
-   */
-  adapter?: runtime.SqlDriverAdapterFactory | null
-  /**
    * Global configuration for omitting model fields by default.
    * 
    * @example
