b5d527a528ce707d001f8aa762a06622b030aebb
DevSammyKad <DevSammyKad@gmail.com>
2025-08-20 13:52:10 +0530

    fix suject page

--- lib/data/subjects/get-all-subjects-by-organization-id.ts
+++ lib/data/subjects/get-all-subjects-by-organization-id.ts
@@ -1,10 +1,9 @@
 'use server';
 
-import { Subject } from '@/generated/prisma';
 import prisma from '@/lib/db';
 import { getOrganizationId } from '@/lib/organization';
 // Get all subjects for the organization
-export async function getAllSubjectsByOrganizationId(): Promise<Subject[]> {
+export async function getAllSubjectsByOrganizationId() {
   const organizationId = await getOrganizationId();
 
   const subjects = await prisma.subject.findMany({
